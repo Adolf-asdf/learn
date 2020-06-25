93ba481b539d055c23ebb196c130416d270c5c34
Adolf-asdf <1778527665@qq.com>
2020-06-25 10:09:58 +0800

    hello world

---
+++ helloWorld.java
@@ -0,0 +1,21 @@
+public class helloWorld {
+	public static void main(String[] args){
+	
+	System.out.println("hello world");
+	System.out.println("hello world1");
+
+	System.out.println("hello world2");
+
+	System.out.println("hello world3");
+
+	System.out.println("hello world4");
+
+	System.out.println("hello world5");
+	System.out.println("hello world6");
+	System.out.println("hello world7");
+
+
+
+	}
+
+}
